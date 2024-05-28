8c525e4cc93cca28c642cb9cf355d76c7f191b9a
Celestine Lee <130682715+Celes0610@users.noreply.github.com>
2024-05-28 11:53:20 -0400

    1st version of drawing the map
    issue: does not show on client connection

--- AnimationPanel.java
+++ AnimationPanel.java
@@ -4,19 +4,42 @@ import java.io.*;
 import javax.imageio.*;
 import java.awt.image.*;
 
-public class AnimationPanel extends JPanel{
-	// Properties
-	
-	// Methods
-	// Override paintCompoent - the way the JPanel is drawn
-	public void paintComponent(Graphics g){
-		super.paintComponent(g);
-	}
+public class AnimationPanel extends JPanel {
+    // Properties
+    private String[][] mapData;
 
+    // Methods
+    // Override paintComponent - the way the JPanel is drawn
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (mapData != null) {
+            drawMap(g);
+        }
+    }
 
-	// Constructor
-	public AnimationPanel(){
-		super();
+    // Method to load map data
+    public void loadMap(String filePath) {
+        mapData = snakeGame.readFile(40, filePath); // Assuming the map has 20 columns
+        repaint();
+    }
 
-	}
+    // Method to draw the map
+    private void drawMap(Graphics g) {
+        for (int i = 0; i < mapData.length; i++) {
+            for (int j = 0; j < mapData[i].length; j++) {
+                if ("f".equals(mapData[i][j])) {
+                    g.setColor(Color.BLACK);
+                    g.fillRect(j * 18, i * 18, 18, 18); // Assuming each cell is 32x32 pixels
+                } else if ("w".equals(mapData[i][j])) {
+                    g.setColor(Color.GREEN);
+                    g.fillRect(j * 18, i * 18, 18, 18);
+                }
+            }
+        }
+    }
+
+    // Constructor
+    public AnimationPanel() {
+        super();
+    }
 }
