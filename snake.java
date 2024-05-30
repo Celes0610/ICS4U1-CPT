59d914a5aa5f47e4c3c19ebec1df16b61385ae73
Celestine Lee <130682715+Celes0610@users.noreply.github.com>
2024-05-29 21:05:57 -0400

    OOP file of snake

---
+++ snake.java
@@ -0,0 +1,43 @@
+import java.awt.Graphics;
+
+public class snake {
+    //properties
+    int[][] segments;
+    char direction;
+    boolean isAlive;
+    int length;
+
+    //methods
+    public void move(){
+
+    }
+
+    public void setDirection(String newDirection){
+
+    }
+
+    public void grow(){
+
+    }
+
+    public boolean collision(){
+        return true;
+    }
+
+    public void drawSnake(Graphics g){
+        
+    }
+
+    public int[] headPos(){
+        return segments[0];
+    }
+
+    public boolean isAlive(){
+        return true;
+    }
+    
+    //constructor
+    public snake(int startX, int StartY, char startDirection){
+
+    }
+}
